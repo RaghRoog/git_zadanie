f2d378297a4fa22588a2a3a0762a7e110ad03a1c
Adriandox <adriandabrowski2002@gmail.com>
2022-11-30 17:32:25 +0100

    Dodano #include <stdio.h> do odczytywania printf i scanf w mainie

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include "zadczwarte.h"
 
 int main(){
-    zadPierwsze();
+//    zadPierwsze();
+    zadanieczwarte();
     printf("Hello wordl");
     return 0;
 }
