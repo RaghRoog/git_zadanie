91328b36a31ede24487330a4f4fabc8976640943
Szymon Pomieciński <szumon612@interia.pl>
2022-11-30 17:48:25 +0100

    dodano ostatni printf, skonczono program

--- zadPierwsze.h
+++ zadPierwsze.h
@@ -3,4 +3,5 @@ void zadPierwsze(){
     float liczba2 = ((float)liczba1);
     printf("Liczba z dokladnoscia do jednego miejsca po przecinku wynosi: %.1f\n", liczba2);
     printf("Liczba z dokladnoscia do trzech miejsc po przecinku wynosi: %.3f\n", liczba2);
+    printf("Liczba w formacie napisowym to: %c\n", liczba1);
 }
